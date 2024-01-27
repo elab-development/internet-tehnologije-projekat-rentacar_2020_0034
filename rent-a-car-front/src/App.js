b72ff4fdff34e4db71ff6369f0c29ee39cc409e7
Eva Milosavljevic <milosavljevic.eva01@gmail.com>
2024-01-27 22:29:54 +0100

    Kreiran navigacioni meni i njegov css

--- rent-a-car-front/src/App.js
+++ rent-a-car-front/src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+
+      </Routes>
+      </BrowserRouter>
       <Footer></Footer>
+      
     </div>
   );
 }
---
+++ rent-a-car-front/src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Pretpostavljamo da postoji CSS fajl za stilizaciju Navbar-a
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="nav-link nav-brand">WheelsOnRent</Link>
+      <div className="nav-links">
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/cars" className="nav-link">Our cars</Link>
+        <Link to="/agents" className="nav-link">Our rental agents</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
