1f1edd7d706d5acf9c2dde0e521fcadc38eac4ff
David Obradovic <david.obadovic3@gmail.com>
2024-01-27 23:56:21 +0100

    Dodata paginacija i pretraga za kola i css

--- rent-a-car-front/src/components/cars/Cars.jsx
+++ rent-a-car-front/src/components/cars/Cars.jsx
@@ -1,21 +1,60 @@
 import React, { useState } from 'react';
 import Car from './Car';
 import useCars from '../customhooks/useCars';
+import './Cars.css'
 
 const Cars = () => {
   
   const [cars] = useCars('http://127.0.0.1:8000/api/cars');
 
+  const [kriterijumPretrage, setKriterijumPretrage] = useState('');
+
+  const handleSearchChange = (e) => {
+    setKriterijumPretrage(e.target.value);
+    setTrenutnaStranica(1); 
+  };
+
+  const filtriranaKola = cars.filter(car =>
+    car.name.toLowerCase().includes(kriterijumPretrage.toLowerCase())
+  );
+
+  const [trenutnaStranica, setTrenutnaStranica] = useState(1);
+  const kolaPoStranici = 6;  
+  const indeksPoslednjeg = trenutnaStranica * kolaPoStranici;
+  const indeksPrvog = indeksPoslednjeg - kolaPoStranici;
+  const trenutnaKola = filtriranaKola.slice(indeksPrvog, indeksPoslednjeg);
+
+  const paginate = (broj) => setTrenutnaStranica(broj);
+
+  const brojeviStranica = [];
+  for (let i = 1; i <= Math.ceil(filtriranaKola.length / kolaPoStranici); i++) {
+    brojeviStranica.push(i);
+  }
+
   return (
     <>
-    <div className="cars">
-      
+     <div className="cars">
+      <div className='cars-search'>
+      <input 
+        type="text" 
+        placeholder="Pretrazite kola po nazivu..." 
+        value={kriterijumPretrage}
+        onChange={handleSearchChange}
+        className="search-input"
+      />
+      </div>
       <div className='cars-section'>
-      {cars.map(car => (
+      {trenutnaKola.map(car => (
         <Car key={car.id} car={car} />
       ))}
       </div>
-      
+      <div className="pagination">
+        {brojeviStranica.map(broj => (
+          <button key={broj} onClick={() => paginate(broj)} className="page-broj">
+            {broj} 
+          </button>
+        ))}
+      </div>
     </div>
     </>
   );
--- rent-a-car-front/src/components/navbar/Navbar.jsx
+++ rent-a-car-front/src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import './Navbar.css'; // Pretpostavljamo da postoji CSS fajl za stilizaciju Nav
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" className="nav-link nav-brand">WheelsOnRent</Link>
+      <Link to="/" className="nav-link nav-brand">Wheels On Rent</Link>
       <div className="nav-links">
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/cars" className="nav-link">Our cars</Link>
