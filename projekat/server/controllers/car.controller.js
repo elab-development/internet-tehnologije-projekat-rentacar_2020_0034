c23684fee622b3d5497f95b122e46e58c3b3e933
David Obadovic <david.obadovic3@gmail.com>
2024-03-01 17:54:51 +0100

    Implementirani Car i User Controller-i

---
+++ projekat/server/controllers/car.controller.js
@@ -0,0 +1,192 @@
+import mongoose from 'mongoose';
+//modeli koji su potrebni
+import Car from '../mongodb/models/car.js';
+import User from '../mongodb/models/user.js';
+
+import * as dotenv from 'dotenv';
+import {v2 as cloudinary} from 'cloudinary';
+
+//biblioteka dotenv učitava .env fajl
+dotenv.config();
+
+//loudinary.config() koja prihvata tri parametra  Ovi parametri se prosleđuju iz .env fajla preko process.env,
+//aplikacija može da koristi Cloudinary API za preuzimanje, otpremanje i manipulisanje slikama
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+
+
+const getAllCars = async (req, res) => {
+    //parametri za filtriranje podataka,ograničavanje broja povratnih podataka i sortiranje podataka
+    const { _end, _order, _start, _sort, title_like = "", carType = ""} = req.query;
+
+    //kasnije se koristi za definisanje različitih parametara za pretraživanje baze podataka
+    const query = {};
+
+    //proverava se "carType" iz zahteva i ako je definisan, dodaje se u "query" objekat kao parametar
+    if(carType !== ""){
+        query.carType = carType;
+    }
+
+    // Pronađeni objekti se dodaju u "query" objekat
+    if(title_like){
+        query.title = {$regex: title_like, $options: 'i' };
+    }
+
+    try {
+        //countDocuments funkcija da bi se izbrojao broj pronađenih koncerata pre slanja upita bazi podataka
+        const count = await Car.countDocuments({query});
+
+        //vracaju se objekti na osnovu zahteva,ogranicenje broja rezultata i da li ima preskakanje, i sortiranje
+        const cars = await Car
+            .find(query)
+            .limit(_end)
+            .skip(_start)
+            .sort({[_sort]: _order})
+
+        // uključuje se informacija o ukupnom broju pronađenih koncerata
+        res.header('x-total-count', count);
+        //Vezano za CORS politiku! Dozvoljava klijentskoj pristup x-total-count, tj.
+        // informaciji o ukupnom broju podataka sto je korisno za paginaciju!
+        res.header('Access-Control-Expose-Headers', 'x-total-count');
+        //sve pronađene objekti se šalju kao JSON odgovor klijentskoj aplikaciji.
+        res.status(200).json(cars);
+    } catch (error) {
+        res.status(500).json({message:error.message}) 
+    }
+};
+
+const getCarDetails = async (req, res) => {
+    //iz parametara zahteva se izdvaja id
+    const { id } = req.params;
+    //da se nadje taj objekat sa tim id-em
+    const carExists = await Car.findOne({
+        _id: id
+    }).populate('creator',); //da se prikaze i kreator objekta
+
+    //salje odgovor sa detaljima objekta
+    if(carExists) { res.status(200).json(carExists) 
+    }else{
+        res.status(404).json({ message: 'Car not found'});
+    }
+};
+
+const createCar = async (req, res) => {
+
+    try {
+        //req.body sadrži parametre za kreiranje objekta koje je korisnik poslao preko HTTP zahteva.
+        const {title, description, carType, location, price, photo, email} = req.body;
+
+    //zapocinje se nova transakcija u bazi podataka
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    //pronađe korisnika na osnovu njihove adrese e-pošte
+    const user = await User.findOne({ email }).session(session);
+
+    if(!user) throw new Error('User not found');
+
+    //servis da bi se slika objekta postavila na mrežu i dobila javni URL.
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    //novu instanca Car modela, koja se zatim dodaje u bazu podataka. 
+    const newCar = await Car.create({
+        title,
+        description,
+        carType,
+        location,
+        price,
+        photo: photoUrl.url,
+        creator: user._id
+    });
+    // ID novog objekta u listu svih koncerata korisnika. Zatim se ovo ažuriranje čuva u bazi podataka.
+    user.allCars.push(newCar._id);
+    await user.save({ session });
+
+    //izvrsava transakciju, promene tokom transakcije se potvrđuju.
+    await session.commitTransaction();
+
+    res.status(200).json({ message: 'Car created succesfully'}) 
+
+    } catch (error) {
+       res.status(500).json({message:error.message}) 
+    }
+   
+
+};
+//editovanje objekta
+const updateCar = async (req, res) => {
+    try {
+        //koji se menja
+        const {id} = req.params;
+        //req.body sadrži parametre za kreiranje objeata koje je korisnik poslao preko HTTP zahteva.
+        const {title, description, carType, location, price, photo} = req.body;
+
+        const photoUrl = await cloudinary.uploader.upload(photo);
+
+        await Car.findByIdAndUpdate({_id: id}, {
+            title,
+            description,
+            carType,
+            location,
+            price,
+            photo: photoUrl.url || photo
+
+        })
+
+        res.status(200).json({message: 'Car updated successfully'})
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+        
+    }
+};
+
+
+//brisanje objekta
+const deleteCar = async (req, res) => {
+    try {
+        //koji objekat se brise
+        const { id } = req.params;
+
+        //koristi za pronalaženje objekta u bazi
+        const carToDelete = await Car.findById({
+            _id: id
+        }).populate('creator'); //učitali podaci korisnika koji je kreirao objekat
+
+        if(!CarToDelete) throw new Error('Car not found'); //ako ne postoji
+
+        //zapocinje se nova transakcija u bazi podataka
+        const session = await mongoose.startSession();
+        session.startTransaction();
+
+        //Uklanjanje objekta iz baze podataka koristeći remove funkciju 
+        carToDelete.remove({session});
+        //uklanja referenca na objekat kod korisnika sa pull funkcijom
+        carToDelete.creator.allCars.pull(CarToDelete);
+
+        //Ažuriranje korisničkog objekta u bazi podataka kako bi se uklonila referenca
+        await carToDelete.creator.save({session});
+        await session.commitTransaction();
+
+        res.status(200).json({message: 'Car deleted successfully'});
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+};
+
+
+
+
+
+export {
+    getAllCars,
+    getCarDetails,
+    createCar,
+    updateCar,
+    deleteCar,
+}
