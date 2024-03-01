7327e6be34799d125091b699f1dfdda758957f75
Eva Milosavljevic <milosavljevic.eva01@gmail.com>
2024-03-01 17:29:59 +0100

    Eksportovane komponente

---
+++ projekat/client/src/components/index.ts
@@ -0,0 +1,24 @@
+
+
+// charts
+import PieChart from './charts/PieChart';
+import CarReferrals from './charts/CarReferrals';
+import TotalRevenue from './charts/TotalRevenue';
+import SatisfactionBox from './charts/SatisfactionBox';
+import CustomButton from './common/CustomButton';
+import CarCard from './common/CarCard';
+import Profile from './common/Profile';
+import ManagerCard from './manager/ManagerCard';
+
+
+export {
+
+  PieChart,
+  CarReferrals,
+  TotalRevenue,
+  SatisfactionBox,
+  CustomButton,
+  CarCard,
+  Profile,
+  ManagerCard
+};
