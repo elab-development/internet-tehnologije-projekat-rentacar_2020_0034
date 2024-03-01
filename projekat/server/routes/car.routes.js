b791eb869766d7344150468fe4aa6b007c43b551
David Obadovic <david.obadovic3@gmail.com>
2024-03-01 17:59:26 +0100

    Jos biblioteka...

---
+++ projekat/server/routes/car.routes.js
@@ -0,0 +1,22 @@
+//Uvoz biblioteke Express koja se koristi za upravljanje rutama i zahtevima
+import express from 'express';
+//funkcije iz kontrolera
+import { createCar,deleteCar,getAllCars,getCarDetails,updateCar} from '../controllers/car.controller.js';
+
+//Kreiranje novog router objekta koji se koristi za definisanje novih ruta
+const router = express.Router();
+
+router.route('/').get(getAllCars);
+
+router.route('/:id').get(getCarDetails);
+
+router.route('/').post(createCar);
+
+//za azuriranje samo odredjenih delova
+router.route('/:id').patch(updateCar);
+
+router.route('/:id').delete(deleteCar);
+
+
+export default router;
+
