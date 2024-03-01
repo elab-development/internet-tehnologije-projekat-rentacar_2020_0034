b5610b3dedfd587c3aa2539229366fc782dd7c62
Eva Milosavljevic <milosavljevic.eva01@gmail.com>
2024-03-01 17:36:05 +0100

    Implementirane konstante i konteksti

---
+++ projekat/client/src/constants/index.ts
@@ -0,0 +1,28 @@
+//koriscene na dashboardu za status barove
+export const carReferralsInfo = [
+    {
+      title: 'Instagram',
+      percentage: 64,
+      color: '#8B0000',
+    },
+    {
+      title: 'Facebook',
+      percentage: 40,
+      color: '#8B0000',
+    },
+    {
+      title: 'TikTok',
+      percentage: 50,
+      color: '#8B0000',
+    },
+    {
+      title: 'YouTube',
+      percentage: 80,
+      color: '#8B0000',
+    },
+    {
+      title: 'Website Baners',
+      percentage: 15,
+      color: '#8B0000',
+    },
+  ];
