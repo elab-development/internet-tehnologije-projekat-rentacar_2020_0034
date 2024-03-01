f4d7457a3190db6d32970f8e0c284caeb36caa61
David Obadovic <david.obadovic3@gmail.com>
2024-03-01 17:45:10 +0100

    Implementirane forme za kreiranje i izmenu odredjene ponude automobila

---
+++ projekat/client/src/pages/create-car.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+import { useGetIdentity } from '@pankod/refine-core';
+
+import { FieldValues, useForm } from '@pankod/refine-react-hook-form';
+
+
+import Form from 'components/common/Form'
+
+
+const CreateCar = () => {
+
+  
+  const { data : user } = useGetIdentity();
+  const [carImage, setCarImage] = useState({name: '', url: ''});
+  //useForm je hook koji se koristi u Refine aplikacijama kako bi se kreirao i validirao formular.
+  const { refineCore: { onFinish, formLoading}, register, handleSubmit} = useForm();
+
+
+  //U handleImageChange se definise funkcija za promenu slike za objekat. 
+  //Ova funkcija prima fajl kao argument, zatim se kreira novi Promise koji koristi FileReader API kako bi se 
+  //pretvorio fajl u Data URL, nakon cega se stanje carImage azurira sa novim imenom i URL-om.
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) => new Promise<string>((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => resolve(fileReader.result as string);
+      fileReader.readAsDataURL(readFile);
+    });
+
+    reader(file).then((result: string) => setCarImage({ name: file?.name, url: result }));
+  };
+
+//U onFinishHandler se definise funkcija koja se poziva kada se formular za kreiranje objekta zavrsi. 
+//Prvo se proverava da li je korisnik dodao sliku za objekat. Ako nije, prikazuje se upozorenje
+  const onFinishHandler = async (data:FieldValues) => {
+    if (!carImage.name) return alert('Please upload an car image');
+    // u suprotnom poziva se onFinish funkcija i salju se podaci formulara, URL slike za objekat i email korisnika.
+    await onFinish({ ...data, photo: carImage.url, email: user.email });
+  };
+
+  return (
+    //Form je komponenta koja se koristi za kreiranje formulara.
+    <Form
+      type="Create"
+      register= {register}
+      onFinish={onFinish}
+      formLoading={formLoading}
+      handleSubmit={handleSubmit}
+      handleImageChange={handleImageChange}
+      onFinishHandler={onFinishHandler}
+      carImage={carImage}
+    />
+  )
+}
+
+export default CreateCar
---
+++ projekat/client/src/pages/edit-car.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { useGetIdentity } from "@pankod/refine-core";
+import { FieldValues, useForm } from "@pankod/refine-react-hook-form";
+import Form from "components/common/Form";
+
+const EditCar = () => {
+    const { data: user } = useGetIdentity();
+    const [carImage, setCarImage] = useState({ name: "", url: "" });
+
+    // onFinish predstavlja funkciju koja se poziva kada se forma pošalje (submit). formLoading je boolean vrednost 
+    //koja označava da li se forma trenutno šalje. register je funkcija koju koristimo za registrovanje polja 
+    //(npr. input elementa) u formi, dok handleSubmit predstavlja funkciju koja se poziva kada se forma pošalje (submit) 
+    //i koja je zadužena za validaciju i slanje podataka sa forme na server.
+    const {
+        refineCore: { onFinish, formLoading },
+        register,
+        handleSubmit,
+    } = useForm();
+
+
+    //U handleImageChange se definise funkcija za promenu slike za objekat. 
+  //Ova funkcija prima fajl kao argument, zatim se kreira novi Promise koji koristi FileReader API kako bi se 
+  //pretvorio fajl u Data URL, nakon cega se stanje carImage azurira sa novim imenom i URL-om.
+    const handleImageChange = (file: File) => {
+        const reader = (readFile: File) =>
+            new Promise<string>((resolve, reject) => {
+                const fileReader = new FileReader();
+                fileReader.onload = () => resolve(fileReader.result as string);
+                fileReader.readAsDataURL(readFile);
+            });
+
+        reader(file).then((result: string) =>
+            setCarImage({ name: file?.name, url: result }),
+        );
+    };
+
+    //U onFinishHandler se definise funkcija koja se poziva kada se formular za kreiranje objekta zavrsi. 
+//Prvo se proverava da li je korisnik dodao sliku za objekat. Ako nije, prikazuje se upozorenje
+    const onFinishHandler = async (data: FieldValues) => {
+        if (!carImage.name) return alert("Please upload a car image");
+
+        //u suprotnom poziva se onFinish funkcija i salju se podaci, URL slike za k i email korisnika.
+        await onFinish({
+            ...data,
+            photo: carImage.url,
+            email: user.email,
+        });
+    };
+
+    return (
+        <Form
+            type="Edit"
+            register={register}
+            onFinish={onFinish}
+            formLoading={formLoading}
+            handleSubmit={handleSubmit}
+            handleImageChange={handleImageChange}
+            onFinishHandler={onFinishHandler}
+            carImage={carImage}
+        />
+    );
+};
+
+export default EditCar;
